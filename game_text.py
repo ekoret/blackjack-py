6c502c3ea549fd2a7083a2d000311f93211c13a8
eko-develops <ekodevelops@gmail.com>
2023-02-22 21:28:14 -0800

    add default font name

--- game_text.py
+++ game_text.py
@@ -8,12 +8,13 @@ class GameText:
     def __init__(self,
                  label_text,
                  font_size=20,
+                 font_name="Arial",
                  text_colour=(255, 255, 255),
                  bg_colour=(0, 0, 0)):
 
         self.label_text = label_text
         self.font_size = font_size
-        self.font = pygame.font.SysFont('Arial', self.font_size)
+        self.font = pygame.font.SysFont(font_name, self.font_size)
         self.label = self.font.render(
             self.label_text, True, text_colour, bg_colour)
 
