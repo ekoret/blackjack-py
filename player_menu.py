36a1f313822a9d17757641670af5b2ba058fa803
eko-develops <ekodevelops@gmail.com>
2023-02-24 12:28:00 -0800

    add method to create list of buttons then draw

--- player_menu.py
+++ player_menu.py
@@ -1,27 +1,36 @@
-import pygame
+from game_button import GameButton
 
 
 class PlayerMenu:
     def __init__(self, game):
         self.game = game
-        self.height = 47
+        self.height = 200
+        self.width = 250
+        self.x_offset = 90
+        self.y_offset = 200
         self.bg_colour = (0, 0, 0)
         self.text_colour = (255, 255, 255)
-        self.x_offset = 80
-        self.y_offset = 200
-        self.width = 225
 
     def draw(self, player):
-        """Menu background"""
-        rect_bg = pygame.Rect(player.x - 80, player.y - 200, 250, 200)
-        pygame.draw.rect(self.game.screen, self.bg_colour, rect_bg)
+        self.x = player.x - self.x_offset
+        self.y = player.y - self.y_offset
+        button_list = self.get_button_list()
 
+        for button in button_list:
+            button.draw(self.game.screen)
+
+    """
+    Creates buttons and sizes + sets coordinates 
+    for them based on the amount of moves in the game settings
+    and for the current player turn
+    """
+
+    def get_button_list(self):
+        button_list = []
+
+        button_height = (self.height // len(self.game.settings.moves))
         for i, move in enumerate(self.game.settings.moves):
-            text_surface = self.game.font.render(
-                move.upper(), True,  self.text_colour, (200, 50, 50))
-            rect = pygame.Rect(player.x - self.x_offset, player.y - self.y_offset,
-                               self.width, (i + 1) * self.height)
-
-            x = rect.x + 10
-            y = rect.y + (rect.height - text_surface.get_height()) // 2
-            self.game.screen.blit(text_surface, (x, y))
+            button = GameButton(
+                self.x, self.y + (i * button_height), self.width, button_height, move, 20, "Arial", (255, 255, 255), (111, 111, 111), (150, 150, 150))
+            button_list.append(button)
+        return button_list
