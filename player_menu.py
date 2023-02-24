a8623be146df9e0b002a0191ba65a24bc4aaca78
eko-develops <ekodevelops@gmail.com>
2023-02-23 23:53:23 -0800

    add black box for menu, comment text

--- player_menu.py
+++ player_menu.py
@@ -4,11 +4,10 @@ import pygame
 class PlayerMenu:
     def draw(self, game, player):
         """Menu heading"""
-        text_surface = game.font.render(
-            "Menu", True, (255, 255, 255))
+        # text_surface = game.font.render("Menu", True, (255, 255, 255))
 
         """Manu background"""
-        rect_bg = pygame.Rect(100, 100, 300, 400)
+        rect_bg = pygame.Rect(player.x - 80, player.y - 200, 250, 200)
 
-        pygame.draw.rect(game.screen, (170, 40, 170), rect_bg)
-        game.screen.blit(text_surface, (100, 300))
+        pygame.draw.rect(game.screen, (0, 0, 0), rect_bg)
+        # game.screen.blit(text_surface, (player.x - 80, player.y - 210))
